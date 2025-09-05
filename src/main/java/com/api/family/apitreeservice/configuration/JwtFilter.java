7aa2de540985de5fd1c4a3319cf4824f746c2d38
daaduu <davaadorj.0510@gmail.com>
2025-09-05 13:08:09 +0800

    code zasvar

--- src/main/java/com/api/family/apitreeservice/configuration/JwtFilter.java
+++ src/main/java/com/api/family/apitreeservice/configuration/JwtFilter.java
@@ -2,6 +2,7 @@ package com.api.family.apitreeservice.configuration;
 
 import com.api.family.apitreeservice.exception.CustomException;
 import com.api.family.apitreeservice.exception.Errors;
+import com.api.family.apitreeservice.repository.UserRepository;
 import com.api.family.apitreeservice.service.CustomerUserDetailService;
 import com.api.family.apitreeservice.service.JwtUtility;
 import io.jsonwebtoken.MalformedJwtException;
@@ -20,37 +21,37 @@ import org.springframework.web.servlet.HandlerExceptionResolver;
 public class JwtFilter extends OncePerRequestFilter {
     private final HandlerExceptionResolver handlerExceptionResolver;
     private final JwtUtility jwtUtility;
-    private final CustomerUserDetailService userDetailService;
+    private final UserRepository userRepository;
 
-    public JwtFilter(HandlerExceptionResolver handlerExceptionResolver, JwtUtility jwtUtility, CustomerUserDetailService userDetailService) {
+    public JwtFilter(HandlerExceptionResolver handlerExceptionResolver, JwtUtility jwtUtility,
+            UserRepository userRepository) {
         this.handlerExceptionResolver = handlerExceptionResolver;
         this.jwtUtility = jwtUtility;
-        this.userDetailService = userDetailService;
+        this.userRepository = userRepository;
     }
 
     @SuppressWarnings("NullableProblems")
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            {
-        try{
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+        try {
             String authHeader = request.getHeader("Authorization");
-            if(SecurityContextHolder.getContext().getAuthentication() == null && authHeader != null
-                    && authHeader.startsWith("Bearer ")){
+            if (SecurityContextHolder.getContext().getAuthentication() == null && authHeader != null
+                    && authHeader.startsWith("Bearer ")) {
                 var token = authHeader.substring(7);
                 var username = jwtUtility.extractUsername(token);
-                UserDetails userDetails = userDetailService.loadUserByUsername(username);
-                if(jwtUtility.validateToken(token, userDetails)){
+                UserDetails userDetails = new CustomerUserDetailService(userRepository).loadUserByUsername(username);
+                if (jwtUtility.validateToken(token, userDetails)) {
                     UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                            userDetails, null, userDetails.getAuthorities()
-                    );
+                            userDetails, null, userDetails.getAuthorities());
                     authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 }
             }
             filterChain.doFilter(request, response);
-        }catch (MalformedJwtException e) {
+        } catch (MalformedJwtException e) {
 
-            handlerExceptionResolver.resolveException(request, response, null, new CustomException(Errors.ACCESS_DENIED, e));
+            handlerExceptionResolver.resolveException(request, response, null,
+                    new CustomException(Errors.ACCESS_DENIED, e));
         } catch (Exception e) {
             handlerExceptionResolver.resolveException(request, response, null, e);
         }
